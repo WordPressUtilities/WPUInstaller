b8bd45904bb5dded1a4ec40979ba16a18ee53568
Darklg <kev.rocher@gmail.com>
2015-09-06 23:16:55 +0200

    Always display default values

--- start.sh
+++ start.sh
@@ -41,7 +41,7 @@ if [[ $project_id == '' ]]; then
 fi;
 echo "- Project ID: ${project_id}";
 
-read -p "What's the project dev url ? " project_dev_url;
+read -p "What's the project dev url ? [http://localhost/${project_id}/] : " project_dev_url;
 if [[ $project_dev_url == '' ]]; then
     project_dev_url="http://localhost/${project_id}/";
 fi;
@@ -51,7 +51,7 @@ read -p "Is it a multilingual project ? (Y/n) " project_l10n;
 if [[ $project_l10n == '' ]]; then
     project_l10n="y";
 else
-    read -p "What's the locale [${WP_LOCALE}]? " user_locale;
+    read -p "What's the locale ? [${WP_LOCALE}] : " user_locale;
     if [[ $user_locale == '' ]]; then
         user_locale="${WP_LOCALE}";
     fi;
@@ -59,31 +59,31 @@ else
     echo "- Locale: ${WP_LOCALE}";
 fi;
 
-read -p "What's your email address ? " email_address;
+read -p "What's your email address ? [test@yopmail.com] : " email_address;
 if [[ $email_address == '' ]]; then
     email_address="test@yopmail.com";
 fi;
 echo "- Email: ${email_address}";
 
-read -p "What's the MYSQL HOST ? " mysql_host;
+read -p "What's the MYSQL HOST ? [127.0.0.1] : " mysql_host;
 if [[ $mysql_host == '' ]]; then
     mysql_host="127.0.0.1";
 fi;
 echo "- MySQL Host: ${mysql_host}";
 
-read -p "What's the MYSQL USER ? " mysql_user;
+read -p "What's the MYSQL USER ? [root] : " mysql_user;
 if [[ $mysql_user == '' ]]; then
     mysql_user="root";
 fi;
 echo "- MySQL User: ${mysql_user}";
 
-read -p "What's the MYSQL PASSWORD ? " mysql_password;
+read -p "What's the MYSQL PASSWORD ? [root] : " mysql_password;
 if [[ $mysql_password == '' ]]; then
     mysql_password="root";
 fi;
 echo "- MySQL Pass: ${mysql_password}";
 
-read -p "What's the MYSQL DATABASE ? " mysql_database;
+read -p "What's the MYSQL DATABASE ? [${project_id}] : " mysql_database;
 if [[ $mysql_database == '' ]]; then
     mysql_database="${project_id}";
 fi;
