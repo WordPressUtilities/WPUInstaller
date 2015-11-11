602c2436f3f8604fd7bde02531dee0c6bff61530
Darklg <kev.rocher@gmail.com>
2015-11-11 22:16:02 +0100

    Reverse "forced" mu-plugins & "classic" mu-plugins. Ask before installing each classic mu-plugin.

--- bin/mu.sh
+++ bin/mu.sh
@@ -8,18 +8,20 @@ echo '### MU-Plugins installation';
 
 mkdir "${MAINDIR}${WP_MUPLUGINS_DIR}";
 
-# Classic MU Plugins
-for i in $WPU_MUPLUGINS
+# Forced MU Plugins
+for i in $WPU_FORCED_MUPLUGINS
 do
     echo "## Install ${i}";
     cp "${MAINDIR}WPUtilities/${WP_MUPLUGINS_DIR}${i}.php" "${MAINDIR}${WP_MUPLUGINS_DIR}${i}.php";
 done;
 
-# Forced MU Plugins
-for i in $WPU_FORCED_MUPLUGINS
+# Classic MU Plugins
+for i in $WPU_MUPLUGINS
 do
-    echo "## Install ${i}";
-    cp "${MAINDIR}WPUtilities/${WP_PLUGINS_DIR}${i}.php" "${MAINDIR}${WP_MUPLUGINS_DIR}${i}.php";
+    read -p "## Install ${i} ? (y/N) " install_muplugin;
+    if [[ $install_muplugin == 'y' ]];then
+        cp "${MAINDIR}WPUtilities/${WP_PLUGINS_DIR}${i}.php" "${MAINDIR}${WP_MUPLUGINS_DIR}${i}.php";
+    fi;
 done;
 
 # Commit Add mu-plugins
--- bin/vars.sh
+++ bin/vars.sh
@@ -10,7 +10,7 @@ WP_LANG_DIR='wp-content/languages/';
 WP_MUPLUGINS_DIR='wp-content/mu-plugins/';
 WP_PLUGINS_DIR='wp-content/plugins/';
 WPU_SUBMODULE_PLUGINS="wpuoptions wpupostmetas wpuseo wpuposttypestaxos wputhumbnails";
-WPU_MUPLUGINS="wpu_body_classes wpu_ux_tweaks";
-WPU_FORCED_MUPLUGINS="wpudisablecomments wpudisablesearch wpudisableposts wputh_admin_protect";
+WPU_FORCED_MUPLUGINS="wpu_body_classes wpu_ux_tweaks";
+WPU_MUPLUGINS="wpudisablecomments wpudisablesearch wpudisableposts wputh_admin_protect";
 
 alias wp="php ${MAINDIR}wp-cli.phar";
