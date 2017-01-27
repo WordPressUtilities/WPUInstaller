d01f5efa9d9ccc03b2aa7fd58ea82ac524508832
Kevin <kevin@rocher-dev.com>
2017-01-27 14:34:44 +0100

    Tools : Add a function to parse json

---
+++ bin/tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# http://stackoverflow.com/a/26655887
+# Parse JSON
+
+function stackov_parse_json()
+{
+    echo $1 | \
+    sed -e 's/[{}]/''/g' | \
+    sed -e 's/", "/'\",\"'/g' | \
+    sed -e 's/" ,"/'\",\"'/g' | \
+    sed -e 's/" , "/'\",\"'/g' | \
+    sed -e 's/","/'\"---SEPERATOR---\"'/g' | \
+    awk -F=':' -v RS='---SEPERATOR---' "\$1~/\"$2\"/ {print}" | \
+    sed -e "s/\"$2\"://" | \
+    tr -d "\n\t" | \
+    sed -e 's/\\"/"/g' | \
+    sed -e 's/\\\\/\\/g' | \
+    sed -e 's/^[ \t]*//g' | \
+    sed -e 's/^"//'  -e 's/"$//'
+}
+
--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ MAINDIR="${PWD}/";
 SCRIPTDIR="$( dirname "${BASH_SOURCE[0]}" )/";
 export PATH=$PATH:/Applications/MAMP/Library/bin/
 
+. "${SCRIPTDIR}bin/tools.sh";
 . "${SCRIPTDIR}bin/vars.sh";
 . "${SCRIPTDIR}bin/questions.sh";
 . "${SCRIPTDIR}bin/git.sh";
