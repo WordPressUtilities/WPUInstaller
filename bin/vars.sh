15e70928d48bb2b0d10c5ddf4304143362ee5074
Kevin <kevin@rocher-dev.com>
2017-10-22 23:54:12 +0200

    MU Plugins - add WPU Settings Version

--- bin/vars.sh
+++ bin/vars.sh
@@ -10,7 +10,7 @@ WP_LANG_DIR='wp-content/languages/';
 WP_MUPLUGINS_DIR='wp-content/mu-plugins/';
 WP_PLUGINS_DIR='wp-content/plugins/';
 WPU_SUBMODULE_PLUGINS="wpuoptions wpupostmetas wpuseo wpuposttypestaxos wputhumbnails";
-WPU_FORCED_MUPLUGINS="wpu_body_classes wpu_ux_tweaks";
+WPU_FORCED_MUPLUGINS="wpu_body_classes wpu_ux_tweaks wpu_settings_version";
 WPU_MUPLUGINS="wpudisablecomments wpudisablesearch wpudisableposts wputh_admin_protect";
 WPU_PHPCLI="${MAINDIR}wp-cli.phar";
 
