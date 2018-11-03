f028a6df302f31f83f47b5359c9418edba756e27
Kevin <kevin@rocher-dev.com>
2018-11-03 16:58:26 +0100

    Vars : remove wputhumbnails + force admin protect + add wputaxometas & wpudisabler.

--- bin/vars.sh
+++ bin/vars.sh
@@ -9,8 +9,7 @@ WP_THEME_DIR='wp-content/themes/';
 WP_LANG_DIR='wp-content/languages/';
 WP_MUPLUGINS_DIR='wp-content/mu-plugins/';
 WP_PLUGINS_DIR='wp-content/plugins/';
-WPU_SUBMODULE_PLUGINS="wpuoptions wpupostmetas wpuseo wpuposttypestaxos wputhumbnails";
-WPU_FORCED_MUPLUGINS="wpu_body_classes wpu_ux_tweaks wpu_settings_version";
-WPU_MUPLUGINS="wpudisablecomments wpudisablesearch wpudisableposts wputh_admin_protect";
+WPU_SUBMODULE_PLUGINS="wpuoptions wpupostmetas wpuseo wpuposttypestaxos wputaxometas wpudisabler";
+WPU_FORCED_MUPLUGINS="wpu_body_classes wpu_ux_tweaks wpu_settings_version wputh_admin_protect";
+WPU_MUPLUGINS="wpudisablecomments wpudisablesearch wpudisableposts";
 WPU_PHPCLI="${MAINDIR}wp-cli.phar";
-
