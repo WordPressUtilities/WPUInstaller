950675946e6183a6b113d9bad4394bb517c107ca
Kevin <kevin@rocher-dev.com>
2019-01-16 17:31:16 +0100

    Git - fix directory + find correct .gitignore file

--- bin/git.sh
+++ bin/git.sh
@@ -4,23 +4,27 @@
 ## Git
 ###################################
 
-## Test git
-
-if [[ ! -d '.git' ]]; then
+## If git
+## https://stackoverflow.com/a/38088814
+wpu_git_is_active="$(git rev-parse --is-inside-work-tree 2>/dev/null)";
+if [[ $wpu_git_is_active != 'true' ]]; then
     git init;
 fi;
 
-## Set gitignore
+# Get root dir
+wpu_git_root_dir="$(git rev-parse --show-toplevel)";
 
+## Set gitignore
 echo '### Set gitignore';
 
-echo "node_modules
+echo "# WordPress
+node_modules
 .sass-cache
 wp-cli.phar
-/WPUtilities/
+WPUtilities/
 /.htaccess
-/wp-content/themes/${project_id}/tests/config.json
-/wp-content/themes/twenty*
-/wp-content/uploads/
-/wp-content/debug.log
-/wp-config.php" >> "${MAINDIR}.gitignore";
+wp-content/themes/${project_id}/tests/config.json
+wp-content/themes/twenty*
+wp-content/uploads/
+wp-content/debug.log
+wp-config.php" >> "${wpu_git_root_dir}/.gitignore";
