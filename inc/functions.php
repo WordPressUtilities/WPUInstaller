eddd37493a4d05ca3f19fc16b776626f12db3037
Kevin <kevin@rocher-dev.com>
2019-04-02 21:51:19 +0200

    Base Theme : rename js files

--- inc/functions.php
+++ inc/functions.php
@@ -68,14 +68,14 @@ function wputh_set_pages_site($pages_site) {
  -------------------------- */
 
 add_filter('wputh_javascript_files', 'wpuproject_javascript_files', 99, 1);
-function wpuproject_javascript_files($files) {
-    unset($files['functions-faq-accordion']);
-    unset($files['functions-search-form-check']);
-    $files['events'] = array(
+function wpuproject_javascript_files($js_files) {
+    unset($js_files['functions-faq-accordion']);
+    unset($js_files['functions-search-form-check']);
+    $js_files['events'] = array(
         'uri' => '/assets/js/events.js',
         'footer' => 1
     );
-    return $files;
+    return $js_files;
 }
 
 /* Styles
