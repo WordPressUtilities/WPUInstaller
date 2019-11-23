3ff47c59075e65d1fd010612dd0dc1a72687f4b2
Kevin <kevin@rocher-dev.com>
2019-11-23 22:20:53 +0100

    Base Functions - Disable REST API for non logged-in users

--- inc/base_functions.php
+++ inc/base_functions.php
@@ -3,3 +3,14 @@
 Plugin Name: [wpuproject] Functions
 Description: Website common functions
 */
+
+/* ----------------------------------------------------------
+  Disable REST API for non logged-in users
+---------------------------------------------------------- */
+
+add_action('plugins_loaded', 'wpuprojectid_functions_plugins_loaded', 5);
+function wpuprojectid_functions_plugins_loaded() {
+    if (!is_user_logged_in()) {
+        add_filter('wpudisabler__disable_wp_api', '__return_true');
+    }
+}
