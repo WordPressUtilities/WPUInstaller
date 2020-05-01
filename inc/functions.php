bcc284e29bb6008501688fae64f08fc2ace5b0d7
Kevin <kevin@rocher-dev.com>
2020-05-01 19:10:19 +0200

    Functions - Disable more widgets

--- inc/functions.php
+++ inc/functions.php
@@ -134,6 +134,12 @@ function wpuproject_unregister_default_widgets() {
     unregister_widget('WP_Widget_Links');
     unregister_widget('WP_Widget_Meta');
     unregister_widget('WP_Widget_Search');
+    unregister_widget('WP_Widget_Media');
+    unregister_widget('WP_Widget_Media_Audio');
+    unregister_widget('WP_Widget_Media_Image');
+    unregister_widget('WP_Widget_Media_Video');
+    unregister_widget('WP_Widget_Media_Gallery');
+    unregister_widget('WP_Widget_Text');
     unregister_widget('WP_Widget_Categories');
     unregister_widget('WP_Widget_Recent_Posts');
     unregister_widget('WP_Widget_Recent_Comments');
