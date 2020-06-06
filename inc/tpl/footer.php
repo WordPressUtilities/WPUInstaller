46c2978c43d5af88d78bbc6d9037b3e6a670143c
Kevin <kevin@rocher-dev.com>
2020-06-06 19:37:42 +0200

    Footer - Load default items.

--- inc/tpl/footer.php
+++ inc/tpl/footer.php
@@ -1,5 +1,10 @@
-<div class="centered-container cc-footer">
+<?php
+$footer_items = wputh_get_menu_items('footer');
+?><div class="centered-container cc-footer">
     <footer class="footer">
         <div class="footer-copyright"><?php echo '&copy; ' . date('Y') . ' - ' . get_bloginfo('name'); ?></div>
+        <?php if ($footer_items): ?>
+        <div class="footer-links"><?php echo '<ul><li>' . implode('</li><li>', $footer_items) . '</li></ul>'; ?></div>
+        <?php endif;?>
     </footer>
 </div>
