9b328c06329888247cf14d0b130d220714869a03
Kevin <kevin@rocher-dev.com>
2020-11-04 21:33:29 +0100

    Child Theme : Use default images for logo & screenshot instead of downloading them

--- bin/theme.sh
+++ bin/theme.sh
@@ -70,8 +70,8 @@ bashutilities_sed "s/wpuproject/${project_name}/g" "${WPU_THEME_TRANSLATE_FILE}"
 # - Assets
 mkdir "${WPU_THEME}assets/";
 mkdir "${WPU_THEME}assets/images";
-wget "https://placehold.it/200x100/fff/000?text=${project_id}" -q -O "${WPU_THEME}assets/images/logo.png";
-wget "https://placehold.it/600x450/fff/000?text=${project_id}" -q -O "${WPU_THEME}screenshot.png";
+cp "${SCRIPTDIR}inc/assets/logo.png" "${WPU_THEME}assets/images/logo.png";
+cp "${SCRIPTDIR}inc/assets/screenshot.png" "${WPU_THEME}screenshot.png";
 mkdir "${WPU_THEME}assets/js";
 cp "${SCRIPTDIR}inc/assets/events.js" "${WPU_THEME}assets/js/events.js";
 
