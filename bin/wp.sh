c628fc7a73f2d4da7711626eb26db48418768225
Kevin <kevin@rocher-dev.com>
2020-11-29 19:09:06 +0100

    Disable file edit in wpconfig

--- bin/wp.sh
+++ bin/wp.sh
@@ -52,6 +52,10 @@ define('WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] . '/');
 define('WP_AUTO_UPDATE_CORE', false);
 define('AUTOMATIC_UPDATER_DISABLED', true);
 
+# Files
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );
+
 # CRONs
 define('DISABLE_WP_CRON', true);
 
