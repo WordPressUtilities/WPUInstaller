d98c831744f9c27c5b3644294922370e835f6b86
Kevin <kevin@rocher-dev.com>
2020-11-29 19:08:14 +0100

    Footer - separate copy

--- inc/tpl/footer.php
+++ inc/tpl/footer.php
@@ -1,8 +1,9 @@
 <?php
+$footer_copyright = '&copy; ' . date('Y') . ' - ' . get_bloginfo('name');
 $footer_items = wputh_get_menu_items('footer');
 ?><div class="centered-container cc-footer">
     <footer class="footer">
-        <div class="footer-copyright"><?php echo '&copy; ' . date('Y') . ' - ' . get_bloginfo('name'); ?></div>
+        <div class="footer-copyright"><?php echo $footer_copyright; ?></div>
         <?php if ($footer_items): ?>
         <div class="footer-links"><?php echo '<ul><li>' . implode('</li><li>', $footer_items) . '</li></ul>'; ?></div>
         <?php endif;?>
