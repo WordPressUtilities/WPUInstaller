66b5c7c156d986e84e207dea36a1af5160645c3b
Kevin <kevin@rocher-dev.com>
2021-02-10 23:07:44 +0100

    Add languages & icons in header

--- inc/tpl/header.php
+++ inc/tpl/header.php
@@ -10,7 +10,8 @@ wp_nav_menu( array(
     'container_class' => 'main-menu__wrapper',
     'menu_class' => 'main-menu'
 ) );
-echo wputh_get_social_links_html();
+echo wputh_get_social_links_html('header__social','icon');
+echo wputh_maincontent_languages();
 ?>
 <a href="#" class="nav-toggle"><span></span></a>
 </header>
