74c5c6ac43b440de8b524ff1a7619049a9784df6
Kevin <kevin@rocher-dev.com>
2021-03-18 21:54:54 +0100

    fix default master include

--- inc/page-master.php
+++ inc/page-master.php
@@ -1,4 +1,4 @@
 <?php
 /* Template Name: Page Master */
 include dirname(__FILE__) . '/../WPUTheme/z-protect.php';
-include get_stylesheet_directory() . '/tpl/master.php';
+include get_stylesheet_directory() . '/tpl/page-master.php';
