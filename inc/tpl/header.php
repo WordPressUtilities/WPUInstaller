80f8c24bc7fd99a93b80e7d3d5c42285e25eeaf5
Kevin <kevin@rocher-dev.com>
2021-08-09 22:33:24 +0200

    Header : Role button for nav toggle

--- inc/tpl/header.php
+++ inc/tpl/header.php
@@ -13,6 +13,6 @@ wp_nav_menu( array(
 echo wputh_get_social_links_html('header__social','icon');
 echo wputh_maincontent_languages();
 ?>
-<a href="#" class="nav-toggle"><span></span></a>
+<a role="button" href="#" class="nav-toggle"><span></span></a>
 </header>
 </div>
