23add0822b1b519de62c687641f131a4b71a0321
Kevin <kevin@rocher-dev.com>
2022-07-30 21:26:29 +0200

    Better Gitignore

--- bin/git.sh
+++ bin/git.sh
@@ -16,32 +16,4 @@ wpu_git_root_dir="$(git rev-parse --show-toplevel)";
 
 ## Set gitignore
 echo '### Set gitignore';
-
-echo "# WordPress
-.sass-cache
-.disable_wpu_admin_protect
-/.htaccess
-/vendor
-/composer.lock
-diff-before.txt
-diff-after.txt
-my.cnf
-node_modules
-package-lock.json
-wp-cli.phar
-yarn.lock
-wp-config.php
-wp-content/advanced-cache.php
-wp-content/cache/
-wp-content/db.php
-wp-content/debug.log
-wp-content/languages/themes/twenty*
-wp-content/object-cache.php
-wp-content/plugins/wp-rocket/license-data.php
-wp-content/themes/twenty*
-wp-content/uploads/
-wp-content/w3tc-config/
-wp-content/wp-rocket-config/
-wpu-test.php
-wpu_local_overrides.php
-WPUtilities/" >> "${wpu_git_root_dir}/.gitignore";
+cat "${SCRIPTDIR}inc/base_gitignore"  >> "${wpu_git_root_dir}/.gitignore";
