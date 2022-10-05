ca44f4ffed9addb9956efcd697a678e5ea26a55e
Kevin <kevin@rocher-dev.com>
2022-10-05 19:56:32 +0200

    Base Header - display button

--- inc/tpl/header.php
+++ inc/tpl/header.php
@@ -1,9 +1,11 @@
 <?php
+$header_button = get_option('wpuprojectid_header_button');
 echo '<div class="cc-header-main__wrapper">';
 echo '<div class="centered-container cc-header-main">';
 echo '<header class="header-main" role="banner">';
 wputh_display_title();
 wputh_display_toggle();
+echo '<div class="header-main__menu"><div class="header-main__menu-inner">';
 wp_nav_menu(array(
     'depth' => 2,
     'theme_location' => 'main',
@@ -14,6 +16,10 @@ wp_nav_menu(array(
 ));
 echo wputh_get_social_links_html('header__social', 'icon');
 echo wputh_maincontent_languages();
+if ($header_button) {
+    echo '<div class="header-main__menu-aside">' . get_wpu_acf_link($header_button, 'wpuprojectid-button wpuprojectid-button--go') . '</div>';
+}
+echo '</div></div>';
 echo '</header>';
 echo '</div>';
 echo '</div>';
