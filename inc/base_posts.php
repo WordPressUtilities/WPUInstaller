0779fc7bbc0c4e020de6d5bc6fc172f8743e565c
Kevin <kevin@rocher-dev.com>
2022-10-24 20:28:27 +0200

    Base Posts - Missed Schedule

--- inc/base_posts.php
+++ inc/base_posts.php
@@ -19,3 +19,32 @@ add_action('init', function () {
         wp_die('Invalid taxonomy');
     }
 });
+
+/* ----------------------------------------------------------
+  Missed Schedule
+---------------------------------------------------------- */
+
+add_action('init', function () {
+    $freq_in_minutes = 2;
+
+    /* Once every N minutes */
+    $chut_posts_missed_schedule_transient = get_transient('chut_posts_missed_schedule_transient');
+    if ($chut_posts_missed_schedule_transient) {
+        return false;
+    }
+    set_transient('chut_posts_missed_schedule_transient', 1, 60 * $freq_in_minutes);
+
+    /* Select all failed transients */
+    global $wpdb;
+    $q = $wpdb->prepare("SELECT * FROM $wpdb->posts WHERE post_status='future' AND post_date_gmt > 0 AND post_date_gmt < UTC_TIMESTAMP()");
+    $missed_ids = $wpdb->get_col($q);
+    if (empty($missed_ids)) {
+        return false;
+    }
+
+    /* Publish these posts */
+    foreach ($missed_ids as $missed_id) {
+        wp_publish_post($missed_id);
+    }
+
+});
