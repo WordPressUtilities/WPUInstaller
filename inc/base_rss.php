a1803c365b71b311b7310faa12cfe5270954711a
Kevin <kevin@rocher-dev.com>
2023-03-18 21:10:21 +0000

    Base RSS : handle author & post type & copyright

--- inc/base_rss.php
+++ inc/base_rss.php
@@ -6,7 +6,7 @@ Description: Config for RSS
 */
 
 /* ----------------------------------------------------------
-  Images in RSS
+  RSS : Images
 ---------------------------------------------------------- */
 
 // display featured post thumbnails in WordPress feeds
@@ -36,3 +36,31 @@ add_action('rss2_item', function () {
         get_post_mime_type($thumbnail_id)
     );
 });
+
+/* ----------------------------------------------------------
+  RSS : Post types
+---------------------------------------------------------- */
+
+add_filter('request', function ($q) {
+    if (isset($q['feed'])) {
+        $q['post_type'] = array('post');
+    }
+    return $q;
+});
+
+/* ----------------------------------------------------------
+  RSS : Author
+---------------------------------------------------------- */
+
+add_filter('the_author', function ($display_name) {
+    if (is_feed()) {
+        return get_bloginfo('name');
+    }
+    return $display_name;
+}, 40);
+
+/* ----------------------------------------------------------
+  RSS : Copyright
+---------------------------------------------------------- */
+
+add_filter('disable__wpuux_add_copyright_feed', '__return_true', 10, 1);
