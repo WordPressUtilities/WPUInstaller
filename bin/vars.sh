8953601610f01307b667d055fd314d427fc85c27
Kevin <kevin@rocher-dev.com>
2023-06-15 16:32:02 +0200

    Mu plugins : force super editor plugin

--- bin/vars.sh
+++ bin/vars.sh
@@ -11,6 +11,6 @@ WP_MUPLUGINS_DIR='wp-content/mu-plugins/';
 WP_PLUGINS_DIR='wp-content/plugins/';
 WPU_SUBMODULE_PLUGINS="wpuoptions wpupostmetas wpuseo wpuposttypestaxos wputaxometas wpudisabler";
 WPU_FORCED_MUPLUGINS="wpu_body_classes ";
-WPU_SUBMODULES_FORCED_MUPLUGINS="wpu_ux_tweaks wpu_file_cache wpu_health_check_tests wpu_settings_version wpu_admin_protect wpuhtaccessprotect";
+WPU_SUBMODULES_FORCED_MUPLUGINS="wpu_ux_tweaks wpu_file_cache wpu_health_check_tests wpu_settings_version wpu_admin_protect wpu_super_editor wpuhtaccessprotect";
 WPU_SUBMODULES_MUPLUGINS="wpudisablesearch wpudisableposts wpudisablecomments wpusimilar";
 WPU_PHPCLI="${MAINDIR}wp-cli.phar";
