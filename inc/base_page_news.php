21393126b5b4aa8c644f5e1b619ddea18e4f1a8a
Kevin <kevin@rocher-dev.com>
2024-04-22 11:02:06 +0200

    Base page news - classname
    - Add current class to news menu item on single posts

--- inc/base_page_news.php
+++ inc/base_page_news.php
@@ -17,3 +17,15 @@ function wpuprojectid_pagenews_wputh_set_pages_site($pages_site) {
     );
     return $pages_site;
 }
+
+/* ----------------------------------------------------------
+  Add current class to news menu item on single posts
+---------------------------------------------------------- */
+
+add_filter('nav_menu_css_class', 'wpuprojectid_pagenews_add_current_page_class', 10, 2);
+function wpuprojectid_pagenews_add_current_page_class($classes, $item) {
+    if (is_singular('post') && $item->object_id == get_option('news__page_id')) {
+        $classes[] = 'current-menu-item';
+    }
+    return $classes;
+}
