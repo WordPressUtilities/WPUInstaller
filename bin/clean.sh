0998377dcc0233dbeae89d91fde7d9f4ec901943
Kevin <kev.rocher@gmail.com>
2024-07-17 18:41:02 +0200

    Clean - Remove some functions

--- bin/clean.sh
+++ bin/clean.sh
@@ -10,3 +10,10 @@ echo '### Clean up';
 rm -rf "${MAINDIR}WPUtilities/";
 rm "${MAINDIR}wp-cli.phar";
 rm "${MAINDIR}my.cnf";
+
+# Functions
+unset -f wpuinstaller_cp_replace;
+unset -f wpuinstaller_github_plugin;
+unset -f wpuinstaller_install_mu;
+unset -f wpuinstaller_install_plugin;
+unset -f wpuinstaller_replace;
